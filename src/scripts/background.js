2fbd87fdb2e60d7ebc6c34d33621fe4942c06e57
Miles Budden <miles@pop-os.localdomain>
2020-05-09 16:58:58 +0100

    The request now goes through

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,19 +1,34 @@
 import ext from "./utils/ext";
+import storage from "./utils/storage";
+storage.get('color', function(resp) {
+  var color = resp.color;
+  if(color) {
+    console.log(color);
+  } else {
+    console.log("No color");
+  }
+});
 
 ext.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if(request.action === "perform-save") {
-      console.log("Extension Type: ", "/* @echo extension */");
-      console.log("PERFORM AJAX", request.data);
       var xhr = new XMLHttpRequest();
-      xhr.open("POST", "http://127.0.0.1:8000/submit_url/1/", true);
-      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-      xhr.withCredentials = true;
+      var FD  = new FormData();
+      FD.append("url", JSON.parse(request.data)["url"]);
+      storage.get('color', function(resp) {
+        // TODO get key
+        FD.append("key", "BYmc4RxDlVMAGijgypImIw");
+      });
+      xhr.open("POST", "http://127.0.0.1:8000/api/", true);
       xhr.onload = function (e) {
         if (xhr.readyState === 4) {
           if (xhr.status === 200) {
             console.log(xhr.responseText);
-            sendResponse({ action: "saved" });
+            if (xhr.responseText == "OK") {
+              sendResponse({ action: "saved" });
+            } else {
+              sendResponse({ action: "not-saved"});
+            }
           } else {
             console.error(xhr.statusText);
             sendResponse({ action: "not-saved" });
@@ -23,7 +38,8 @@ ext.runtime.onMessage.addListener(
       xhr.onerror = function (e) {
         console.error(xhr.statusText);
       };
-      xhr.send("url=" + request.data.url); 
+      xhr.send(FD); 
     }
+  return true;
   }
 );
